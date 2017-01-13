5bf37b4efda98682a4f4290e5dff0fc411246558
andrey9828 <aaa73731919@gmail.com>
2017-01-13 05:42:57 +0200

    Added support CORS and few fixes

--- toDoListBackend/routes/index.js
+++ toDoListBackend/routes/index.js
@@ -1,3 +1,7 @@
 module.exports = function ( app ) {
   require('./tasks')(app);
+
+  app.get('/api', function (req, res) {
+    res.send('API is running');
+  });
 };
--- toDoListBackend/routes/tasks.js
+++ toDoListBackend/routes/tasks.js
@@ -1,21 +1,22 @@
-module.exports = function ( app ) {
-  var log       = require('../libs/log')(module);
+module.exports = function (app) {
+  var log = require('../libs/log')(module);
   var TaskModel = require('../libs/mongoose').TaskModel;
-  var bodyParser = require('body-parser');
 
-  app.get('/api/tasks', function(req, res) {
+  app.get('/api/tasks', function (req, res) {
     return TaskModel.find(function (err, tasks) {
       if (!err) {
         return res.send(tasks);
       } else {
         res.statusCode = 500;
-        log.error('Internal error(%d): %s',res.statusCode,err.message);
-        return res.send({ error: 'Server error' });
+        log.error('Internal error(%d): %s', res.statusCode, err.message);
+        return res.send({
+          error: 'Server error'
+        });
       }
     });
   });
 
-  app.post('/api/tasks', function(req, res) {
+  app.post('/api/tasks', function (req, res) {
     var task = new TaskModel({
       title: req.body.title,
       description: req.body.description
@@ -24,41 +25,58 @@ module.exports = function ( app ) {
     task.save(function (err) {
       if (!err) {
         log.info("task created");
-        return res.send({ status: 'OK', task: task });
+        return res.send({
+          status: 'OK',
+          task: task
+        });
       } else {
         console.log(err);
-        if(err.name == 'ValidationError') {
+        if (err.name == 'ValidationError') {
           res.statusCode = 400;
-          res.send({ error: 'Validation error' }); } else {
+          res.send({
+            error: 'Validation error'
+          });
+        } else {
           res.statusCode = 500;
-          res.send({ error: 'Server error' });
+          res.send({
+            error: 'Server error'
+          });
         }
-        log.error('Internal error(%d): %s',res.statusCode,err.message);
+        log.error('Internal error(%d): %s', res.statusCode, err.message);
       }
     });
   });
 
-  app.get('/api/tasks/:id', function(req, res) {
+  app.get('/api/tasks/:id', function (req, res) {
     return TaskModel.findById(req.params.id, function (err, task) {
-      if(!task) {
+      if (!task) {
         res.statusCode = 404;
-        return res.send({ error: 'Not found' });
+        return res.send({
+          error: 'Not found'
+        });
       }
       if (!err) {
-        return res.send({ status: 'OK', task:task });
+        return res.send({
+          status: 'OK',
+          task: task
+        });
       } else {
         res.statusCode = 500;
-        log.error('Internal error(%d): %s',res.statusCode,err.message);
-        return res.send({ error: 'Server error' });
+        log.error('Internal error(%d): %s', res.statusCode, err.message);
+        return res.send({
+          error: 'Server error'
+        });
       }
     });
   });
 
-  app.put('/api/tasks/:id', function (req, res){
+  app.put('/api/tasks/:id', function (req, res) {
     return TaskModel.findById(req.params.id, function (err, task) {
-      if(!task) {
+      if (!task) {
         res.statusCode = 404;
-        return res.send({ error: 'Not found' });
+        return res.send({
+          error: 'Not found'
+        });
       }
 
       task.title = req.body.title;
@@ -66,35 +84,48 @@ module.exports = function ( app ) {
       return task.save(function (err) {
         if (!err) {
           log.info("task updated");
-          return res.send({ status: 'OK', task:task });
+          return res.send({
+            status: 'OK',
+            task: task
+          });
         } else {
-          if(err.name == 'ValidationError') {
+          if (err.name == 'ValidationError') {
             res.statusCode = 400;
-            res.send({ error: 'Validation error' });
-            } else {
-              res.statusCode = 500;
-              res.send({ error: 'Server error' });
-            }
-          log.error('Internal error(%d): %s',res.statusCode,err.message);
+            res.send({
+              error: 'Validation error'
+            });
+          } else {
+            res.statusCode = 500;
+            res.send({
+              error: 'Server error'
+            });
+          }
+          log.error('Internal error(%d): %s', res.statusCode, err.message);
         }
       });
     });
   });
 
-  app.delete('/api/tasks/:id', function (req, res){
+  app.delete('/api/tasks/:id', function (req, res) {
     return TaskModel.findById(req.params.id, function (err, task) {
-      if(!task) {
+      if (!task) {
         res.statusCode = 404;
-        return res.send({ error: 'Not found' });
+        return res.send({
+          error: 'Not found'
+        });
       }
       return task.remove(function (err) {
         if (!err) {
           log.info("task removed");
-          return res.send({ status: 'OK' });
+          return res.send({
+            status: 'OK'
+          });
         } else {
           res.statusCode = 500;
-          log.error('Internal error(%d): %s',res.statusCode,err.message);
-          return res.send({ error: 'Server error' });
+          log.error('Internal error(%d): %s', res.statusCode, err.message);
+          return res.send({
+            error: 'Server error'
+          });
         }
       });
     });
